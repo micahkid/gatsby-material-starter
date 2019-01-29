b1cfa85cbb2a5345f4f82969e76991db222a92cb
micahkid <16616575+micahkid@users.noreply.github.com>
2019-01-29 13:54:15 -0500

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -42,22 +42,4 @@ module.exports = {
   copyright: "Copyright © 2019. Micah Yaple Design" // Copyright string for the footer of the website and RSS feed.
 };
 
-// Validate
 
-// Make sure pathPrefix is empty if not needed
-if (config.pathPrefix === "/") {
-  config.pathPrefix = "";
-} else {
-  // Make sure pathPrefix only contains the first forward slash
-  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
-}
-
-// Make sure siteUrl doesn't have an ending forward slash
-if (config.siteUrl.substr(-1) === "/")
-  config.siteUrl = config.siteUrl.slice(0, -1);
-
-// Make sure siteRss has a starting forward slash
-if (config.siteRss && config.siteRss[0] !== "/")
-  config.siteRss = `/${config.siteRss}`;
-
-module.exports = config;
