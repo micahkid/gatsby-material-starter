ec0ffe9d8f21753f4f206d1b5a2e6a2e038c3ce5
micahkid <16616575+micahkid@users.noreply.github.com>
2019-01-29 13:58:40 -0500

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "Micah Yaple Design", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://micahyaple.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "/micah-yaple-design", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "Micah Yaple Design - Design & marketing for freelancers.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
