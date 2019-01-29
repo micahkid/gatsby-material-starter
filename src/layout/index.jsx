6a50e18019320c43fb47e3488571832975ad1ac9
micahkid <16616575+micahkid@users.noreply.github.com>
2019-01-29 11:37:16 -0500

    fav icon still not working

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,7 +5,6 @@ import Navigation from "../components/Navigation";
 import config from "../../data/SiteConfig";
 import "./index.scss";
 import "./global.scss";
-import largeFavicon from "../../img/favicon.png";
 
 
 export default class MainLayout extends React.Component {
@@ -16,10 +15,6 @@ export default class MainLayout extends React.Component {
         <div>
           <Helmet>
             <meta name="description" content={config.siteDescription} />
-            link={[
-        { rel: 'icon', type: 'image/png', href: `${largeFavicon}` },
-       
-      ]}
           </Helmet>
           {children}
         </div>
